867cd9635a106567c7498be0d4b112dc0c5c2a66
Indu Ramesh <induramesh@gmail.com>
2019-07-22 18:54:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,22 @@
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  Welcome, #{name}. You are number 1 in line.
+katz_deli = []
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
 end 
 
 def line(deli)
   if deli.size == 0 
     puts "The line is currently empty.".
   else
-    puts 
+    deli.map |name| do
+      puts "#{deli.index(name) + 1}. name"
+    end
+  end 
+end 
+
+def now_serving(deli)
+   if deli.size == 0 
+    puts "The line is currently empty.".
+  else
+    
