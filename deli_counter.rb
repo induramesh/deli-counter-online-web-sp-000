a0660c13076fa30dd2112d372b0287f922173eda
Indu Ramesh <induramesh@gmail.com>
2019-07-22 18:50:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
-katz_deli = []
-
-def take_a_number(line, name)
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  Welcome, #{name}. You are number 1 in line.
 end 
+
 def line(deli)
   if deli.size == 0 
     puts "The line is currently empty.".
