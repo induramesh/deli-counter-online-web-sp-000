b172553d9ba2c82fcd28c082c648ee3a515effbe
Indu Ramesh <induramesh@gmail.com>
2019-07-22 19:12:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(deli)
   if deli.size == 0 
     puts "The line is currently empty."
   else #there are people in line
-    puts "The line is currently: #{deli.map { |name| "#{deli.index(name) + 1}. #{name}" }}""
+    puts "The line is currently: #{deli.map { |name| "#{deli.index(name) + 1}. #{name}" }}"
   end 
 end 
 
